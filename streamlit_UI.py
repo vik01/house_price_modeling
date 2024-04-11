d157487a128a3867bf70c7b255248ed7f755b13c
Vikram Bhatt <vikrambhatt4817@gmail.com>
2024-04-11 15:00:38 -0400

    finished project

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -10,24 +10,24 @@ bath=st.number_input("Number of Bathroms", min_value=1)
 gen_predictions = st.button('Generate Predicted Cost')
 
 if gen_predictions:
-    predicted_price = sqft*662 - bed*458430 + bath*971200 # replace with final prediction model
+    predicted_price = sqft*662 + bed*458430 + bath*971200 # replace with final prediction model
     st.subheader('Estimated Cost of your Dream Home: '+"$"+f'{round(predicted_price):,}')
     #st.divider()
 
     if sqft>1000:
-        predicted_price_sqft = (sqft-500)*662 - bed*458430 + bath*971200
+        predicted_price_sqft = (sqft-500)*662 + bed*458430 + bath*971200
         percent_saved_sqft = (predicted_price_sqft-predicted_price)/predicted_price*100
         st.metric(label="Estimated Cost of a Smaller Property Size", value=f"${(predicted_price_sqft):,}", delta= f"{percent_saved_sqft:.2f}%", delta_color='inverse')
         st.text(f'By looking at a smaller property with {sqft-500} square feet, you can save: ${(predicted_price-predicted_price_sqft):,}')
 
     if bed>1:
-        predicted_price_bed = sqft*662 - (bed-1)*458430 + bath*971200
+        predicted_price_bed = sqft*662 + (bed-1)*458430 + bath*971200
         percent_saved_bed = (predicted_price_bed-predicted_price)/predicted_price*100
         st.metric(label="Estimated Cost of a Property with Fewer Bedrooms", value=f"${(predicted_price_bed):,}", delta= f"{percent_saved_bed:.2f}%", delta_color='inverse')
         st.text(f'By looking at a property with {bed-1} bedrooms, you can save: ${(predicted_price-predicted_price_bed):,}')
     
     if bath>1:
-        predicted_price_bath = sqft*662 - bed*458430 + (bath-1)*971200
+        predicted_price_bath = sqft*662 + bed*458430 + (bath-1)*971200
         percent_saved_bath = (predicted_price_bath-predicted_price)/predicted_price*100
         st.metric(label="Estimated Cost of a Property with Fewer Bathrooms", value=f"${(predicted_price_bath):,}", delta= f"{percent_saved_bath:.2f}%", delta_color='inverse')
         st.text(f'By looking at a property with {bath-1} Bathrooms, you can save: ${(predicted_price-predicted_price_bath):,}')
